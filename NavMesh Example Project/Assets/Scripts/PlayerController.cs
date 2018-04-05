05cc50f44afd51bc9e3100ac9c88a3d52639b4da
drtrifle <drtrifle@gmail.com>
2018-04-05 16:18:05 +0800

    Completed Example 1 Scene

--- NavMesh Example Project/Assets/Scripts/PlayerController.cs
+++ NavMesh Example Project/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class PlayerController : MonoBehaviour {
 
+    private Camera mainCam;
+    private NavMeshAgent agent;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        mainCam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                agent.SetDestination(hit.point);
+            }
+        }
 	}
 }
