1e3cd1c23aaff094959ada79fb5fdaef5ad0061f
KenWhy <drtrifle@gmail.com>
2018-05-20 14:48:05 +0800

    Respawn when Falling out of World & Changed Jump Animation

---
+++ Laurasia/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Respawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ResetRotationAndPositionToStart() {
+        transform.rotation = startRotation;
+        transform.position = startPosition;
+    }
+
+    private void OnTriggerEnter(Collider colli) {
+        if (colli.CompareTag("Death")) {
+            ResetRotationAndPositionToStart();
+        }
+    }
+}
